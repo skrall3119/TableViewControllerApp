56f96ceb319f2ae17137dce99ad6b2092289b8d1
skrall3119 <alexjanci@yahoo.com>
2021-10-27 12:58:03 -0500

    innit

---
+++ TableViewControllerApp/TableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TableViewController.swift
+//  TableViewControllerApp
+//
+//  Created by Alex Janci on 10/27/21.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let petArray = ["cat", "dog", "parakeet", "parrot", "canary", "finch", "tropical fish", "goldfish", "sea horses", "hamster", "gerbil", "rabbit", "turtle", "snake", "lizard", "hermit crab"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID")
+        cell?.textLabel?.text = petArray[indexPath.row]
+        return cell!
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedItem = petArray[indexPath.row]
+        let alert = UIAlertController(title: "Your Choice", message: "\(selectedItem)", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {action -> Void in})
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+
+}
